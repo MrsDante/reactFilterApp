35500bcb34e0ad241388373d8a3489b1d92cb599
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-10-27 19:31:16 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Movie from './Movie';
+import Filter from './Filter';
+
 
 function App() {
+
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=e220c7eebbb3e36db046e5b0d37adb2a&language=en-US&page=1')
+    const movies = await data.json();
+
+    //console.log(movies.results)
+
+    setPopular(movies.results);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Filter />
+      <div className="popular-movies">
+        {popular.map(movie => {
+          return <Movie key={movie.id} movie={movie} />
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+//e220c7eebbb3e36db046e5b0d37adb2a
+
+//https://api.themoviedb.org/3/movie/550?api_key=e220c7eebbb3e36db046e5b0d37adb2a
+
+/*
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjIwYzdlZWJiYjNlMzZkYjA0NmU1YjBkMzdhZGIyYSIsInN1YiI6IjYzNWFhNDkwODk0ZWQ2MDA5MTQzNWU1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R9KGCioOM1FHnA3arKxLJhNsKmW1lzLVzTJ-GfRLw5g
+*/
+
+
+//https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
---
+++ src/Filter.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+function Filter() {
+    return (
+        <div className="filter-container">
+          <button>All</button>
+          <button>Comedy</button>
+          <button>Horror</button>
+        </div>
+    );
+}
+
+export default Filter;
